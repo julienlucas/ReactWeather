bd39eb09da982bf6c26fda5f5f3060795c4f0984
jules84v <contact@libres-et-heureux.com>
2017-08-07 16:59:31 +0200

    Style about page & link to ressources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,13 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About Component (yes for true!)</h3>
-      <p>Welcome to about page, yes!</p>
+      <h1 className="text-center">About Component</h1>
+      <p>Ceci est une application React conçue en React. J'ai construis cette application pour apprendre à créer des applications React. Contactez-moi si vous êtes intéréssé pour que nous créeons votre app React ensemble.</p>
+      <p>Voici quelque outils que j'utilise:</p>
+      <ul>
+        <li><a href="https://facebook.github.io/react">React</a> - ceci était le JavaScript utilisé.</li>
+        <li><a href="https://openweathermap.org">Open Weather Map</a> - ceci est l'API que j'ai utilisé pour les températures.</li>
+      </ul>
     </div>
   )
 };
